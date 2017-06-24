bd7464834d4b96f511642e89d94d24df05e6300b
Dimitri Simon <dimitri.simon@addmore-mobile.de>
2017-06-24 21:58:49 +0200

    added homefragment

---
+++ app/src/main/java/com/example/dimitrisimon/gcpicturepicker/fragments/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.dimitrisimon.gcpicturepicker.fragments
+
+import android.app.Fragment
+
+/**
+ * (c) Dimitri Simon on 24.06.17
+ */
+
+class HomeFragment : Fragment() {
+
+
+
+
+}
