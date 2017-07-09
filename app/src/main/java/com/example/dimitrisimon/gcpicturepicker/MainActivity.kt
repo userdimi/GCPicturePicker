10a452bdf4cf22b97c24c6ca68a931d7cf14bc90
Dimitri Simon <dimitri.simon@addmore-mobile.de>
2017-07-09 17:09:39 +0200

    onRequestPermission in Homefragment fixed

--- app/src/main/java/com/example/dimitrisimon/gcpicturepicker/MainActivity.kt
+++ app/src/main/java/com/example/dimitrisimon/gcpicturepicker/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.dimitrisimon.gcpicturepicker
 
-
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.example.dimitrisimon.gcpicturepicker.fragments.HomeFragment
 import com.example.dimitrisimon.gcpicturepicker.fragments.HomeFragment.*
+import com.example.dimitrisimon.gcpicturepicker.helper.PermissionHelper
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +26,27 @@ class MainActivity : AppCompatActivity() {
      */
     private fun  startHomeFragment() {
         val homeFragment = fragmentManager.findFragmentByTag(statics.TAG) ?: HomeFragment()
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, homeFragment, statics.TAG)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, homeFragment as Fragment?, statics.TAG)
                 .commit()
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when (requestCode) {
+
+            PermissionHelper.statics.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                    -> if (grantResults.isNotEmpty() && grantResults [0] == PackageManager.PERMISSION_GRANTED) {
+                            val fragments: MutableList<android.support.v4.app.Fragment>? = supportFragmentManager.fragments
+                if (fragments != null) {
+                    for (fragment in fragments) {
+                        fragment.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                    }
+                }
+            }
+
+            else -> return
+
+        }
+    }
 }
--- app/src/main/java/com/example/dimitrisimon/gcpicturepicker/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/dimitrisimon/gcpicturepicker/fragments/HomeFragment.kt
@@ -1,6 +1,6 @@
 package com.example.dimitrisimon.gcpicturepicker.fragments
 
-import android.app.Fragment
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,13 +12,13 @@ import com.example.dimitrisimon.gcpicturepicker.fragments.HomeFragment.statics.T
 import com.example.dimitrisimon.gcpicturepicker.helper.PermissionHelper
 import com.example.dimitrisimon.gcpicturepicker.helper.StorageHelper
 import kotlinx.android.synthetic.main.fragment_home.*
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.support.v4.toast
 
 /**
  * (c) Dimitri Simon on 24.06.17
  */
 
-class HomeFragment : Fragment() {
+class HomeFragment : android.support.v4.app.Fragment() {
 
     /**
      * Static vars
@@ -63,10 +63,10 @@ class HomeFragment : Fragment() {
         if (Build.VERSION.SDK_INT > 23) {
 
             // now check if the read external storage permission is already available.
-            if (PermissionHelper().permissionToReadStorageNotGranded(activity)) {
+            if (PermissionHelper().permissionToReadStorageNotGranded(context)) {
 
                 // if not, request permissions and open gallery
-                PermissionHelper().requestReadExternStoragePermission(activity)
+                PermissionHelper().requestReadExternStoragePermission(context)
 
             } else {
                 //permissions, available, open gallery
@@ -75,7 +75,20 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun openGallery() {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PermissionHelper.statics.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+            -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+
+                openGallery()
+            }
+
+            else -> return
+        }
+    }
+
+    fun openGallery() {
         toast("Open Gallery")
     }
+
 }
